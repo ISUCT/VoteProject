bbf24b26f8d094b627a5ce1ccd9dd4ad6cc5bf8f
jskonst <jskonst@yandex.ru>
2013-07-21 00:51:14 +0400

    Локальные изменения на моей машине

--- BaseProject/settings.py
+++ BaseProject/settings.py
@@ -6,9 +6,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',   # Add 'postgresql_psycopg2',  'mysql',  'sqlite3' or 'oracle'.
         'NAME': os.path.join(os.path.dirname(__file__), 'mydata.db').replace('\\', '/'),  # Or path to database file if using sqlite3.
-        'USER': '',  # 'ivpy',                      # Not used with sqlite3.
-        'PASSWORD': '',  # 'tf0uqJidWtfYoDZraM',                  # Not used with sqlite3.
-        'HOST': '',  # 'ivpy.ru',                      # Set to empty string for localhost. Not used with sqlite3.
+        'USER': '',                        # Not used with sqlite3.
+        'PASSWORD': '',                    # Not used with sqlite3.
+        'HOST': '',                        # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
