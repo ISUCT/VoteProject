ce4eb36b0950b41ede1c97a571eaef6358de8573
jskonst <jskonst@yandex.ru>
2013-10-03 16:35:47 +0400

    Добавленаоснова для форм

--- department/views.py
+++ department/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+# -*- coding=utf8 -*-
+from django.shortcuts import render_to_response, redirect
+from django.template.context import RequestContext
+from django.http import HttpResponseRedirect
+from department.forms import FacultyForm
+#from models import Section, Doklad
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+def FacultyAdd(request):
+    #создаем экземпляр отображаемой формы (forms.py)
+    facultyForm = FacultyForm(request.POST)
+    #Возвращаем пользователю страничку в ответ на запрос (шаблон, form подменяем на созданную выше форму, остальное просто надо)
+    return render_to_response('dokladedit.html', { 'form': facultyForm}, context_instance=RequestContext(request))
+
