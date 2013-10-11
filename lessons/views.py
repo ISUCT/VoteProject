b57a6ce32d9dc9d4c173e4a2a43162aabf6d5e04
Kseniya <kseniya@kseniya-To-be-filled-by-O-E-M.(none)>
2013-10-11 22:05:09 +0400

    Добавлена форма к занятию

--- lessons/views.py
+++ lessons/views.py
@@ -4,10 +4,19 @@ from django.shortcuts import render, get_object_or_404, render_to_response
 from django.template.context import RequestContext
 from django.core.urlresolvers import reverse
 from lessons.forms import LessonForm
+from models import Lesson
 
 def addLesson(request):
-    lessonForm = LessonForm(request.POST)
-    return render_to_response('dokladedit.html', { 'form': lessonForm}, context_instance=RequestContext(request))
+    user = request.user
+    if user.is_authenticated():
+        lessonForm = LessonForm(request.POST)
+        if request.method == 'POST':
+            lesson = Lesson()
+            if lessonForm.is_valid():
+                lesson.name = lessonForm.cleaned_data['group', 'discipline', 'dateAndTtime', 'room', 'lessonID', 'status', 'user']
+                lesson.save()
+                return HttpResponseRedirect('/admin')
+        return render_to_response('dokladedit.html', { 'form': lessonForm}, context_instance=RequestContext(request))
     #text = """<html><body><h1>Hello world</h1><p>asdfgvbasdbg</p></body></html>"""
     #text2 = "hello again"
     #return HttpResponse(text)
