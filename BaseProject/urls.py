06c585136a90421577223339d7851d92b5e12b28
jskonst <jskonst@yandex.ru>
2013-11-22 20:47:41 +0400

    fix before branch

--- BaseProject/urls.py
+++ BaseProject/urls.py
@@ -17,21 +17,21 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
 
     #url(r'^test/', 'testForms.views.contact'),
-    url(r'^accounts/', include('accounts.urls')),
+    #url(r'^accounts/', include('accounts.urls')),
     # Подключаем урлы приложения конференция
     #url(r'^accounts/', include('conference.urls')),
     #url(r'^', include('conference.urls')),
-    url(r'^', include('accounts.urls')),
+    #url(r'^', include('accounts.urls')),
     url(r'^', include('polls.urls')),
     url(r'^', include('lessons.urls')),
     url(r'^', include('department.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/', include('registration.urls')),
+    #url(r'^accounts/', include('registration.urls')),
     #url(r'^$', 'BaseProject.views.home'),  # Основная страница с информацией
-    url(r'^news/$', 'BaseProject.views.news'),  # Новости
-    url(r'^contact/$', 'BaseProject.views.contacts'),  # Контакты
-    url(r'^schedule/$', 'BaseProject.views.schedule'),
-    url(r'^agreement/$', 'BaseProject.views.agreement'),
+    #url(r'^news/$', 'BaseProject.views.news'),  # Новости
+    #url(r'^contact/$', 'BaseProject.views.contacts'),  # Контакты
+    #url(r'^schedule/$', 'BaseProject.views.schedule'),
+    #url(r'^agreement/$', 'BaseProject.views.agreement'),
     #чтобы правильно отображать статику на сервере
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
