7e854dbf679060c257036065a5b2014fb5ae31d8
jskonst <jskonst@altsoft.biz>
2013-12-23 19:30:59 +0400

    auth

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,18 @@
 # coding: utf8
 # Create your views here.
 #import glob
-from django.shortcuts import render_to_response, render
+from django.shortcuts import render
 #from django.http import HttpResponse
 from django.template.context import RequestContext
+from django.contrib.auth import logout
 
 
 def logout(request):
-    text = "leave so soon?"
+    user = request.user #запрашиваем пользователя из запроса (сессии)
+    text="not auth"
+    if user.is_active:
+        logout(request)
+        text = "leave so soon?"
     #return render_to_response('test.html', {'content': text}, RequestContext(request))
     return render(request,'test.html', {'content': text})
 
